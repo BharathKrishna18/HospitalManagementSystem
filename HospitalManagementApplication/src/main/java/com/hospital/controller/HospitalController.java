8a2c89031ab473836e0e77e30ba15c6f080d88eb
BCoder18 <bk1832004@gmail.com>
2025-11-01 09:20:04 +0530

    Initial Commit

---
+++ HospitalManagementApplication/src/main/java/com/hospital/controller/HospitalController.java
@@ -0,0 +1,15 @@
+package com.hospital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HospitalController {
+
+	@GetMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("welcome", "Welcome to ABC Hospital");
+		return "welcome";
+	}
+}
