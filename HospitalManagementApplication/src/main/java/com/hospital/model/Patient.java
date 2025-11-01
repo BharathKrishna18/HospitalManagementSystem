0ae27d181c8def60be5539b5f4631b643eb4aa5e
mariy <mariy@AetherOfAribah>
2025-11-01 14:50:05 +0530

    Patient.java

--- HospitalManagementApplication/src/main/java/com/hospital/model/Patient.java
+++ HospitalManagementApplication/src/main/java/com/hospital/model/Patient.java
@@ -44,5 +44,118 @@ public class Patient
 		return patientId;
 	}
 	
+	public void setPatientId(Long patientId)
+	{
+		this.patientId = patientId;
+	}
 	
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getEmail() 
+	{
+		return email;
+	}
+
+	public void setEmail(String email) 
+	{
+		this.email = email;
+	}
+
+	public String getPassword() 
+	{
+		return password;
+	}
+
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+
+	public LocalDate getDob() 
+	{
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) 
+	{
+		this.dob = dob;
+	}
+
+	public String getGender() 
+	{
+		return gender;
+	}
+
+	public void setGender(String gender) 
+	{
+		this.gender = gender;
+	}
+
+	public Double getWeight() 
+	{
+		return weight;
+	}
+
+	public void setWeight(Double weight) 
+	{
+		this.weight = weight;
+	}
+
+	public Double getHeight() 
+	{
+		return height;
+	}
+
+	public void setHeight(Double height) 
+	{
+		this.height = height;
+	}
+
+	public String getBloodGroup() 
+	{
+		return bloodGroup;
+	}
+
+	public void setBloodGroup(String bloodGroup) 
+	{
+		this.bloodGroup = bloodGroup;
+	}
+
+	public String getAllergies() 
+	{
+		return allergies;
+	}
+
+	public void setAllergies(String allergies) 
+	{
+		this.allergies = allergies;
+	}
+
+	public String getPreviousMedicalHistory() 
+	{
+		return previousMedicalHistory;
+	}
+
+	public void setPreviousMedicalHistory(String previousMedicalHistory) 
+	{
+		this.previousMedicalHistory = previousMedicalHistory;
+	}
 }
